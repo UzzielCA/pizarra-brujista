3774c7a206d941bed24405fd2d12bd532ffbdabf
Uzziel Cervantes <uzziel.cervantes@banregio.com>
2025-03-17 20:33:44 -0600

    feat: add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet]
+  imports: [RouterOutlet, NavBarComponent]
 })
 export class AppComponent {
   title = 'pizarra-brujista';
